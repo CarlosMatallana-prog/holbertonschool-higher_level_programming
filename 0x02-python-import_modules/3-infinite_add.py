3c72e7d4b4b16ad523a9f914a97b6285efd89ec6
CarlosMatallana-prog <3496@holbertonschool.com>
2021-09-08 19:50:01 -0500

    :hammer: prints the result of the addition of all arguments

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    nargs = len(argv)
+    if nargs == 1:
+        print("0")
+    elif nargs == 2:
+        print("{}".format(argv[1]))
+    elif nargs > 2:
+        result = 0
+        for argument in argv[1:]:
+            result = result + int(argument)
+            print(result)
