bfaf271f809e6268c4b6ce2f2b8c9d82271c4066
CarlosMatallana-prog <3496@holbertonschool.com>
2021-09-08 19:43:19 -0500

    :hammer: prints the number of and the list of its arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args = len(sys.argv)
+
+    if args == 2:
+        print("1 argument:".format(args - 1))
+        print("1: {:s}".format(sys.argv[1]))
+    elif args > 2:
+        print("{:d} arguments:".format(args - 1))
+        counter = 1
+        while counter < args:
+            print("{:d}: {:s}".format(counter, sys.argv[counter]))
+            counter += 1
+        else:
+            print("0 arguments.")
