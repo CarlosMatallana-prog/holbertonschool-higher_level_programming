ac6c455bef32dc9dc8912118c6bc7dcbe64a7adf
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-17 11:15:23 -0500

    :hammer: Prints the first x elements of a list and only integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    c = 0
+    while (i < x):
+        try:
+            if type(my_list[i]) == int:
+                print("{}".format(my_list[i]), end='')
+                c = c + 1
+            i = i + 1
+        except (ValueError, TypeError):
+            pass
+
+    print()
+    return c
