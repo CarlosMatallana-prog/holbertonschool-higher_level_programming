f7294ed89e75cbf8bfc9f742da401a274f768b4c
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-17 13:00:37 -0500

    :bug: Fix output error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-    result = 0
     try:
-        result = a / b
-        return result
+        a / b
     except:
         return None
     finally:
-        print("Inside result: {}".format(result))
+        print("Inside result: {}".format(a / b))
