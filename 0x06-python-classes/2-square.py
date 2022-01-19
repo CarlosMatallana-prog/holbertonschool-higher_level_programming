49bd66fa0eb5389571dab5b3cc6e5bfaeeb22d31
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-19 08:52:24 -0500

    :hammer: Update validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initializes the data."""
-        if type(size) != int:
+        if isinstance(size, (int)) is False:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise TypeError("size must be >= 0")
