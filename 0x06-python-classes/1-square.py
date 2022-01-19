54a5439beee47946a71d74dca3998c797e12e5e7
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-19 08:31:06 -0500

    :hammer: Add the Square class that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""An empty Square class"""
+
+
+class Square:
+    """Defines a class Square"""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.size = size
