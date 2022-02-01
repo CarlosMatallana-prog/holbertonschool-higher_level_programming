1d9414d6cca5efdcbcd0db9edee68db28b201779
CarlosMatallana-prog <3496@holbertonschool.com>
2022-01-31 20:48:29 -0500

    :hammer: add base class BaseGeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" BaseGeometry Class """
+
+
+class BaseGeometry():
+    """ BaseGeometry Class """
+
+    def area(self):
+        """ raises an Exception with a message  """
+        raise Exception("area() is not implemented")
