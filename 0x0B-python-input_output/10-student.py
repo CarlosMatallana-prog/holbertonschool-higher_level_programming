d5592e6f270d2b005356856b55dc5aa5dbb9d8b2
CarlosMatallana-prog <3496@holbertonschool.com>
2022-02-01 20:37:20 -0500

    :hammer: Add class Student that defines a student

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -3,27 +3,19 @@
 
 
 class Student():
-    """ Studen class, with age, first and last name. """
+    """ Student class """
     first_name = ""
     last_name = ""
     age = ""
 
     def __init__(self, first_name, last_name, age):
-        """ __init__ :
-            Args:
-                first_name(str): Student's first_name
-                last_name(str): Student's last_name
-                age(str): Student's age
-        """
+        """ __init__ function """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """ Returns the dictionary representation of a Student instance
-            Returns:
-                dictionary representation of the Student
-        """
+        """ Returns the dictionary representation of a Student """
         if type(attrs) is list:
             new_dict = {}
             for key in attrs:
