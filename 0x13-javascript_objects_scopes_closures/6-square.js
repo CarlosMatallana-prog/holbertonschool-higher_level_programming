7ca5fef561f05aaee10431e0751539bae1f25c34
CarlosMatallana-prog <3496@holbertonschool.com>
2022-04-07 17:31:11 -0500

    :hammer: Add charPrint method, prints the square with the character C

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    c = typeof c === 'undefined' ? 'X' : 'C';
+    for (let i = this.height; i > 0; i--) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
