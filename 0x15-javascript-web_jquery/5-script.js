d2858300c407e4b185c4628d9754f116dfe09d4f
CarlosMatallana-prog <3496@holbertonschool.com>
2022-04-13 13:25:51 -0500

    :hammer: add item on click

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  const listItems = $('.my_list');
+  const addItem = $('#add_item');
+  addItem.click(function () {
+    const newItem = '<li>Item</li>';
+    listItems.append(newItem);
+  });
+};
