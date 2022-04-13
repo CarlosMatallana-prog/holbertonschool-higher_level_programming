9c7ef598e558b2d5aaecf1fb30bcad990637d7b0
CarlosMatallana-prog <3496@holbertonschool.com>
2022-04-13 13:47:39 -0500

    :hammer: Fetch star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  const url = "https://swapi-api.hbtn.io/api/people/5/?format=json";
+  $.get(url, (data, status) => {
+    if (status === "success") {
+      $("#character").text(data.name);
+    }
+  });
+});
